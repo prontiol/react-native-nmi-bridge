da7e041fbed7c6d46a24baf62c042b08cb8ee981
Denys Mikhalenko <denys@prontiol.com>
2017-09-25 12:32:53 +0200

    Drop redundant files

--- index.android.js
+++
@@ -1,9 +0,0 @@
-import React from 'react-native';
-
-const ReactNativeNmiBridge = React.NativeModules.ReactNativeNmiBridge;
-
-export default {
-  reactNativeNmiBridge: (onSuccess, onFailure) => {
-    return ReactNativeNmiBridge.reactNativeNmiBridge(onSuccess, onFailure);
-  },
-};
